f9d20bed5670610a4e3b02e8784882d574bdd7c9
Iryna Kvachuk <kvachukir@gmail.com>
2024-01-18 20:56:12 +0200

    baseTypes

---
+++ src/stores/_common/types/baseTypes.ts
@@ -0,0 +1,7 @@
+export type DispatchT<A> = (value: A) => void;
+export type SetStateActionT<S> = S | ((prevState: S) => S);
+
+export type ButtonClickT = {
+  event: React.MouseEvent<HTMLButtonElement>;
+  setIsLoading: DispatchT<SetStateActionT<boolean>>;
+};
